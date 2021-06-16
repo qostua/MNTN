6d52460d1f8f621456cbc9f72ec9b751f2293f60
qostua <qostua@gmail.com>
2021-06-17 00:08:35 +0300

    Активный пункт в пагинации менияется при прокрутке

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,17 @@ let articlesCoordinate = function (i) {
   let y = mainPageArticles[i].getBoundingClientRect().top + window.pageYOffset;
   return y;
 }
+
+let articlesWindowCoordinate = function (i) {
+  let y = mainPageArticles[i].getBoundingClientRect().top;
+  return y;
+}
+
+let articlesHeight = function (i) {
+  let h = mainPageArticles[i].getBoundingClientRect().height;
+  return h;
+}
+
 let paginationClick = function (item, i) {
   item.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -21,6 +32,27 @@ let paginationClick = function (item, i) {
     }
   });
 }
+
+let paginationScroll = function (item, i) {
+  window.addEventListener('scroll', function () {
+    if (i == 0) {
+      if (window.pageYOffset >= articlesCoordinate(i) - 100) {
+        item.classList.remove('pagination-right__item--active')
+      } else {
+        item.classList.add('pagination-right__item--active')
+      }
+    } else {
+      if (articlesWindowCoordinate(i - 1) <= 100 && articlesWindowCoordinate(i - 1) > - articlesHeight(i - 1) - 100) {
+        item.classList.add('pagination-right__item--active')
+      } else {
+        item.classList.remove('pagination-right__item--active')
+      }
+    }
+  });
+}
+
+
 for (let i = 0; i < paginationItems.length; i++) {
   paginationClick(paginationItems[i], i);
+  paginationScroll(paginationItems[i], i);
 }
